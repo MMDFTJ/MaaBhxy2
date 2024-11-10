6195fed8f69c464863d9676ff54058fbd8453ae7
MMDFTJ <73114181+MMDFTJ@users.noreply.github.com>
2024-11-10 11:38:37 +0800

    更新了UI

--- ui/bhxy_UI.py
+++ ui/bhxy_UI.py
@@ -35,45 +35,40 @@ class Ui_Form(object):
         self.gridLayout_4.setContentsMargins(0, 0, 0, 0)
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.stopPushButton = PushButton(self.layoutWidget)
+        self.stopPushButton.setObjectName(u"stopPushButton")
+
+        self.gridLayout.addWidget(self.stopPushButton, 0, 6, 1, 1)
+
         self.BodyLabel = BodyLabel(self.layoutWidget)
         self.BodyLabel.setObjectName(u"BodyLabel")
 
         self.gridLayout.addWidget(self.BodyLabel, 0, 0, 1, 1)
-
-        self.scanAdbPushButton = PushButton(self.layoutWidget)
-        self.scanAdbPushButton.setObjectName(u"scanAdbPushButton")
-
-        self.gridLayout.addWidget(self.scanAdbPushButton, 0, 1, 1, 1)
-
-        self.adbAddressEditableComboBox = EditableComboBox(self.layoutWidget)
-        self.adbAddressEditableComboBox.setObjectName(u"adbAddressEditableComboBox")
-
-        self.gridLayout.addWidget(self.adbAddressEditableComboBox, 0, 2, 1, 1)
 
         self.adbIpPortEditableComboBox = EditableComboBox(self.layoutWidget)
         self.adbIpPortEditableComboBox.setObjectName(u"adbIpPortEditableComboBox")
 
         self.gridLayout.addWidget(self.adbIpPortEditableComboBox, 0, 3, 1, 1)
 
-        self.adbConnectPushButton = PushButton(self.layoutWidget)
-        self.adbConnectPushButton.setObjectName(u"adbConnectPushButton")
+        self.adbAddressEditableComboBox = EditableComboBox(self.layoutWidget)
+        self.adbAddressEditableComboBox.setObjectName(u"adbAddressEditableComboBox")
 
-        self.gridLayout.addWidget(self.adbConnectPushButton, 0, 4, 1, 1)
+        self.gridLayout.addWidget(self.adbAddressEditableComboBox, 0, 2, 1, 1)
 
         self.BodyLabel_2 = BodyLabel(self.layoutWidget)
         self.BodyLabel_2.setObjectName(u"BodyLabel_2")
 
-        self.gridLayout.addWidget(self.BodyLabel_2, 0, 5, 1, 1)
+        self.gridLayout.addWidget(self.BodyLabel_2, 0, 4, 1, 1)
+
+        self.scanAdbPushButton = PushButton(self.layoutWidget)
+        self.scanAdbPushButton.setObjectName(u"scanAdbPushButton")
+
+        self.gridLayout.addWidget(self.scanAdbPushButton, 0, 1, 1, 1)
 
         self.startPushButton = PushButton(self.layoutWidget)
         self.startPushButton.setObjectName(u"startPushButton")
 
-        self.gridLayout.addWidget(self.startPushButton, 0, 6, 1, 1)
-
-        self.stopPushButton = PushButton(self.layoutWidget)
-        self.stopPushButton.setObjectName(u"stopPushButton")
-
-        self.gridLayout.addWidget(self.stopPushButton, 0, 7, 1, 1)
+        self.gridLayout.addWidget(self.startPushButton, 0, 5, 1, 1)
 
 
         self.gridLayout_4.addLayout(self.gridLayout, 0, 0, 1, 1)
@@ -401,7 +396,7 @@ class Ui_Form(object):
 
         self.retranslateUi(Form)
 
-        self.settingStackedWidget.setCurrentIndex(1)
+        self.settingStackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Form)
@@ -409,16 +404,15 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.stopPushButton.setText(QCoreApplication.translate("Form", u"\u505c\u6b62", None))
         self.BodyLabel.setText(QCoreApplication.translate("Form", u"Adb\u8fde\u63a5\uff1a", None))
-        self.scanAdbPushButton.setText(QCoreApplication.translate("Form", u"\u626b\u63cf\u8bbe\u5907", None))
+        self.adbIpPortEditableComboBox.setPlaceholderText(QCoreApplication.translate("Form", u"adb ip:port", None))
         self.adbAddressEditableComboBox.setInputMask("")
         self.adbAddressEditableComboBox.setText("")
         self.adbAddressEditableComboBox.setPlaceholderText(QCoreApplication.translate("Form", u"adb\u5730\u5740", None))
-        self.adbIpPortEditableComboBox.setPlaceholderText(QCoreApplication.translate("Form", u"adb ip:port", None))
-        self.adbConnectPushButton.setText(QCoreApplication.translate("Form", u"\u8fde\u63a5", None))
         self.BodyLabel_2.setText(QCoreApplication.translate("Form", u"\u8c03\u5ea6\u5668\uff1a", None))
+        self.scanAdbPushButton.setText(QCoreApplication.translate("Form", u"\u5237\u65b0\u8bbe\u5907", None))
         self.startPushButton.setText(QCoreApplication.translate("Form", u"\u5f00\u59cb", None))
-        self.stopPushButton.setText(QCoreApplication.translate("Form", u"\u505c\u6b62", None))
         self.screenshotPushButton.setText(QCoreApplication.translate("Form", u"\u622a\u56fe\u5de5\u5177", None))
         self.BodyLabel_3.setText(QCoreApplication.translate("Form", u"\u4efb\u52a1\uff1a", None))
         self.tasksComboBox.setText("")
@@ -426,7 +420,7 @@ class Ui_Form(object):
         self.doubleCheckBox_clear.setText(QCoreApplication.translate("Form", u"\u53cc\u500d\u5238", None))
         self.BodyLabel_5.setText(QCoreApplication.translate("Form", u"\u5237\u53d6\u6b21\u6570\uff1a", None))
         self.repetitionsLineEdit_clear.setText(QCoreApplication.translate("Form", u"6", None))
-        self.PlainTextEdit.setPlainText(QCoreApplication.translate("Form", u"\u52fe\u9009\u6e05300\u4f53\u529b\u4f1a\u5728\u5b8c\u6210\u5d29\u79d1\u540e\u81ea\u52a8\u627e\u5230\u6d3b\u52a8\u5173\u5361\u5237\u4e09\u6b21BONUS\u5173\n"
+        self.PlainTextEdit.setPlainText(QCoreApplication.translate("Form", u"\u52fe\u9009\u6e05300\u4f53\u529b\u81ea\u52a8\u627e\u5230\u6d3b\u52a8\u5173\u5361\u5237\u4e09\u6b21BONUS\u5173\n"
 "\n"
 "\u5982\u679c\u60f3\u7528\u53cc\u500d\u5238\u7684\u8bdd\u628a\u5237\u53d6\u6b21\u6570\u586b\u62103\n"
 "", None))
